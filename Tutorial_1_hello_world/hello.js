151a791c19edbf647810666e587c580622476d49
Pijus Sarker <pijus.sarker@outbounders.com>
2015-11-18 00:42:21 -0500

    Tutorial : Hello Node.js

--- Tutorial_1_hello_world/hello.js
+++ Tutorial_1_hello_world/hello.js
@@ -15,7 +15,7 @@ http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text/plain'});
 
     // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    response.end('Hello Node.js\n');
 }).listen(8001);
 // this server only listen to port number 8001
 
